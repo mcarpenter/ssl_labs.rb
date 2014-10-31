93b9bbc8a6c60572c438ae99a3b2438f7204cff0
Martin Carpenter <mcarpenter@free.fr>
2014-10-31 22:00:29 +0100

    Fix EndpointData unit test

--- test/test_endpoint_data.rb
+++ test/test_endpoint_data.rb
@@ -8,8 +8,7 @@ class TestEndpointData < Test::Unit::TestCase
 
   def test_constructor
     json = File.read(File.join(File.dirname(__FILE__), 'endpoint_data.json'))
-    hash = JSON.parse(json)
-    epd = SslLabs::EndpointData.from_hash(hash)
+    epd = SslLabs::EndpointData.from_json(json)
     # XXX insert tests here
     #binding.pry
   end
